956b89122bc8824cbf2608c99846d817032d1aef
dubianets <vadikdubianets@gmail.com>
2020-11-03 14:56:29 -0800

    new version

--- src/modules/user/Model.js
+++ src/modules/user/Model.js
@@ -5,8 +5,15 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: String,
-  password: String,
+  email:{
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
 
 export default mongoose.model('User', userSchema);
--- src/modules/user/Routes.js
+++ src/modules/user/Routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import userRegister from './userRegister';
+import userGetAll from './userGetAll';
 
 const router = Router();
 router.post('/', userRegister); // POST localhost:5000/user/
+router.get('/', userGetAll);
 
 export default router;
